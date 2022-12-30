f244b5cbd9d5864b9ab6ac0843d090c175bd01fe
vastqud <66101476+vastqud@users.noreply.github.com>
2022-12-29 20:36:23 -0500

    df

--- warmerproject/main.c
+++ warmerproject/main.c
@@ -10,14 +10,27 @@ static const uint8_t FEATURE = 0x1D;//0b11101;
 static const uint8_t DYNPD = 0x1C;//0b11100;
 static const uint8_t CONFIG = 0x00;//0b00000;
 static const uint8_t FIFO_STATUS = 0x17;//0b10111;
+static const uint8_t RF_CHNL = 0x05;
 
 static const uint8_t WRITE_COMMAND_WORD = 0x20; //0b00100000
 static const uint8_t READ_COMMAND_WORD = 0x00;
 
-void read_status(spi_inst_t *spi, const uint cs, uint8_t *buf) {
-    gpio_put(cs, 0); //high to low transition
-    spi_read_blocking(spi, 0, buf, 1); //read & write the data to buffer
-    gpio_put(cs, 1); //back to high
+void write_to_register(spi_inst_t *spi, const uint cs, const uint8_t reg, uint8_t data[], int mbytes) {
+    mbytes++;
+    data[0] = 0x20 | reg; //first byte (write command)
+
+    gpio_put(cs, 0);
+    spi_write_blocking(spi, data, mbytes);
+    gpio_put(cs, 1);
+}
+
+void read_register(spi_inst_t *spi, const uint cs, const uint8_t reg, uint8_t *buf, int mbytes) {
+    uint8_t msg = 0x00 | reg;
+
+    gpio_put(cs, 0);
+    spi_write_blocking(spi, &msg, 1); //send read command
+    spi_read_blocking(spi, 0, buf, mbytes); //read one byte from spi to buf array
+    gpio_put(cs, 1);
 }
 
 void init_board_led() {
@@ -28,41 +41,37 @@ void init_board_led() {
     gpio_put(LED_PIN, 1);
 }
 
-void write_register_command(spi_inst_t *spi, const uint cs, uint8_t addr, uint8_t data) {
-    uint8_t msg[2];
+void set_defaults(spi_inst_t *spi, const uint cs) {
+    uint8_t rf_data[2] = {0, 0x06};
+    uint8_t feature_data[2] = {0, 0x06};
+    uint8_t dynpd_data[2] = {0, 0x03};
+    uint8_t rf_chnl_data[2] = {0, 0x7A};
 
-    msg[0] = WRITE_COMMAND_WORD | addr; //set command word
-    msg[1] = data;
+    uint8_t addr_p0_data[6] = {0, 0xF2, 0xF2, 0xF2, 0xF2, 0xF2};
+    uint8_t addr_p1_data[6] = {0, 0xE3, 0xE3, 0xE3, 0xE3, 0xE3};
+    uint8_t tx_addr_data[6] = {0, 0xF2, 0xF2, 0xF2, 0xF2, 0xF2};
 
-    gpio_put(cs, 0);
-    spi_write_blocking(spi, msg, 2); //send the write command word + data bits
-    gpio_put(cs, 1);
+    write_to_register(spi, cs, RF_SETUP, rf_data, 1); 
+    write_to_register(spi, cs, FEATURE, feature_data, 1);
+    write_to_register(spi, cs, DYNPD, dynpd_data, 1);
+    write_to_register(spi, cs, RF_CHNL, rf_chnl_data, 1);
+
+    write_to_register(spi, cs, RX_ADDR_P0, addr_p0_data, 5);
+    write_to_register(spi, cs, RX_ADDR_P1, addr_p1_data, 5);
+    write_to_register(spi, cs, TX_ADDR, tx_addr_data, 5);
 }
 
 void set_rx(spi_inst_t *spi, const uint cs, const uint ce) {
-    write_register_command(spi, cs, CONFIG, 0x7B);
-    gpio_put(ce, 1);
-}
+    uint8_t config_data[2] = {0, 0x7B};
 
-void set_defaults(spi_inst_t *spi, const uint cs) { //configures registers for a RX device
-    write_register_command(spi, cs, RF_SETUP, 0x06); //set RF_SETUP register
-    write_register_command(spi, cs, FEATURE, 0x06); //set FEATURE register
-    write_register_command(spi, cs, DYNPD, 0x03); //set DYNPD data
-}
-
-void read_register(spi_inst_t *spi, const uint cs, uint8_t addr, uint8_t *buf, const uint8_t nbytes) {
-    uint8_t msg = READ_COMMAND_WORD | addr;
-
-    gpio_put(cs, 0); //high to low transition
-    spi_write_blocking(spi, &msg, 1); //send read command word (1 byte)
-    spi_read_blocking(spi, 0, buf, nbytes); //read & write the data to buffer
-    gpio_put(cs, 1); //back to high
+    write_to_register(spi, cs, CONFIG, config_data, 1); //configure CONFIG register
+    gpio_put(ce, 1); //set CE high
 }
 
 int main() {
     uint csn = 17, ce = 22, mosi = 19, sck = 18, miso = 16;
     spi_inst_t *spi = spi0;
-    uint8_t data_buffer[5];
+    uint8_t buffer[5];
 
     stdio_init_all();
     init_board_led();
@@ -82,8 +91,8 @@ int main() {
 
     spi_set_format(spi0,   //spi port 0
                     8,      //8 bits per transfer
-                    1,      
-                    1,      
+                    0,      
+                    0,      
                     SPI_MSB_FIRST);
 
     gpio_set_function(sck, GPIO_FUNC_SPI);
@@ -91,19 +100,15 @@ int main() {
     gpio_set_function(miso, GPIO_FUNC_SPI);
 
     sleep_ms(103);
-
-    //set_rx(spi, csn, ce);
-    write_register_command(spi, csn, CONFIG, 0x7B);
-    write_register_command(spi, csn, FEATURE, 0x06);
-    //write_register_command(spi, csn, DYNPD, 0x03);
-    //set_defaults(spi, csn);
+    set_defaults(spi, csn);
+    set_rx(spi, csn, ce);
 
     while (1) {
-        read_register(spi, csn, CONFIG, data_buffer, 1);
+        read_register(spi, csn, RF_CHNL, buffer, 1);
+        
         int i = 0;
-
         for (i = 0; i <= 4; i++) {
-            printf("%d ", data_buffer[i]);
+            printf("%d ", buffer[i]);
         }
         printf("\n");
 
@@ -112,9 +117,6 @@ int main() {
 }
 
 /*
-read works kind of. when it works, it prints the target byte as the second byte in the buffer instead of the first.
-also, it only appears to print the most recently written byte to any register.
-
 0x06         RF_SETUP   0x06 00110
 0xF2F2F2F2F2 RX_ADDR_P0 0x0A 01010
 0xE3E3E3E3E3 RX_ADDR_P1 0x0C 01100
@@ -134,15 +136,5 @@ test RX device:
 read FIFO_STATUS 0x17 10111 register
 should see: 00010001 / 0x11 / 17
 
-10001011
 cp warmerproject.uf2 /h/
-12 0 12 0 12
-1100 0000 1100 0000 1100
-
-00001110
-
-00000000
-00000110
-
-00010000
 */
